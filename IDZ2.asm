ca7d07c221bc752ee844b372f2581fa8afeb46ab
klyshnikov <86251037+klyshnikov@users.noreply.github.com>
2023-11-05 23:29:46 +0300

    Add files via upload

---
+++ IDZ2.asm
@@ -0,0 +1,185 @@
+.data
+one: .double 1
+ten: .double 10
+zr : .double 0
+m_one : .double -1
+percents: .double 0.05
+msg1: .asciz "\nEnter x - between -1 and 1: "
+msg2: .asciz "\nError! Wrong array size!"
+msg3: .asciz "\nCurrent value: 1/(1-x) ~= "
+
+.text
+fld ft0, percents, t0             # Get precents from data
+addi sp, sp, -4				# Count epsilon
+fsd ft0, (sp)				# Store percent data
+jal create_epsilon			# ft0 - actual parametr (percents)
+fmv.d fs1, fa0				# Move result to fs1
+
+li a7, 4                      	  # Enter a welcome message
+la a0, msg1
+ecall
+
+addi sp, sp, -4			        # Let user enter x
+jal enter_x			        # No actual parametr
+fmv.d fs0, fa0 			  	# Move result to fs0
+fld ft10, zr, t0		  # Read 0 to ft10
+addi sp, sp, -4				# Check x
+fsd fa0, (sp)				# fa0 - actual parametr (x)
+jal check_x
+fgt.d t0, fa0, ft10               # Check answer, if it return 0, call ERROR 
+beqz t0, ERROR_SIZE
+
+li a7, 4                          # Enter answer message
+la a0, msg3
+ecall
+
+addi sp, sp, -4                         # Count answer
+fsd fs0, (sp)                           # fs0, fs1 - actual parametrs (x, epsilon)
+addi sp, sp, -4
+fsd fs1, (sp)
+jal count
+
+li a7, 3			 	# Result just in fa0 - show it;
+ecall
+
+j end_program
+
+.text 
+enter_x:                 #void enter_x(double x)
+	# Without registers
+	addi sp, sp, -4
+	sw ra, (sp)
+
+	li a7, 7
+	ecall
+	
+	lw ra, (sp)
+	ret
+	
+.text
+check_x:                #bool check_x(double x)
+	# ft0 = x
+	# ft1 = res1 
+	# ft2 = -1
+	# ft3 = 1
+	# ft4 = res2
+	# ft10 = 0
+	addi sp, sp, -16
+	sw ra, (sp)
+	addi sp, sp, 16
+	fld ft0, (sp)
+	addi sp, sp, -4
+	
+	
+	fld ft3, one, t0
+	fld ft10, zr, t0
+	fmv.d ft2, ft10
+	fsub.d ft2, ft2, ft3
+	
+	fsd ft0, (sp)
+	addi sp, sp, -4
+	fsd ft2, (sp)
+	addi sp, sp, -4
+	fsd ft3, (sp)
+	addi sp, sp, -4
+	
+	fgt.d t0, ft0, ft2
+	fgt.d t1, ft3, ft0
+	
+	fmv.d fa0, ft10
+	bgtz t0, cond1
+	j no_cond
+	
+	cond1:
+		bgtz t1, cond2
+		j no_cond
+	cond2:
+		fmv.d fa0, ft3
+		
+	no_cond:
+	
+	lw ra, (sp)
+	ret
+	
+	
+.text
+create_epsilon:  	# double create_epsilon(double percents)
+	# ft0 - percents
+	# ft1 = 10
+	# ft2 = 1
+	addi sp, sp, -4
+	sw ra, (sp)
+
+	addi sp, sp, 4
+	fld ft0, (sp)
+	addi sp, sp, -4
+	fld ft1, ten, t0
+	fld ft2 one, t0
+	fdiv.d ft0, ft0, ft1
+	fdiv.d ft0, ft0, ft1
+	fadd.d ft0, ft0, ft2
+	fmv.d fa0, ft0
+
+	lw ra, (sp)
+	ret
+	
+.text
+count:   		#double count(double x, double epsilon)
+	# ft0 = x
+	# ft1 = epsilon
+	# ft3 = sum_prev
+	# ft4 = sum_current
+	# ft5 = sum_current/sum_prev
+	# ft6 = x^(current_div)
+	# ft7 = epsilon-1
+	# ft8 = 1
+	
+	addi sp, sp, -20
+	sw ra, (sp)
+	
+	addi sp, sp, 20
+	fld ft1, (sp)
+	addi sp, sp, 4
+	fld ft0, (sp)
+	addi sp, sp, -8
+	
+	fld ft3, one, t0
+	fld ft6, one, t0
+	fld ft8, one, t0
+	fadd.d ft7, ft8, ft8
+	fsub.d ft7, ft7, ft1
+	
+	fsd ft0, (sp)
+	addi sp, sp, -4
+	fsd ft1, (sp)
+	addi sp, sp, -4
+	fsd ft7, (sp)
+	addi sp, sp, -4
+	fsd ft8, (sp)
+	addi sp, sp, -4
+	
+	
+	loop:
+		fmul.d ft6, ft6, ft0
+		fmv.d ft4, ft3
+		fadd.d ft4, ft4, ft6
+		fdiv.d ft5, ft4, ft3
+		fmv.d ft3, ft4
+		flt.d t0, ft5, ft1
+		fgt.d t1, ft5, ft7
+		mul t0, t0, t1
+		
+		beqz t0, loop
+		
+	fmv.d fa0, ft4
+	lw ra, (sp)
+	ret
+
+	
+.text
+ERROR_SIZE:           # If x not in (-1, 1)
+li a7, 4
+la a0, msg2
+ecall
+end_program:          # Jump here to end program
+
